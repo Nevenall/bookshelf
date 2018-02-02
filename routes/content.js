db0a68adb73422e04c8da753d783e3ddf5ad0d72
Dan Behlings <nevenall@hotmail.com>
2018-02-02 12:58:32 -0800

    just a comment

--- routes/content.js
+++ routes/content.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
 
   // we probably don't need to render a view here, 
   // just return the indcated html fragment. 
-  
+  res.send("hello from content router");
 
 });
 
