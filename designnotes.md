9d59ce5296b164d88f6c08c724d86c313c7f4a52
Dan Behlings <nevenall@hotmail.com>
2018-02-03 18:24:31 -0800

    content module

--- designnotes.md
+++ designnotes.md
@@ -1,1 +1,3 @@
 make the file content to nav data a module? maybe there's already a npm package for that?
+
+offcanvas nav for pages, there is bootstrap example 
---
+++ src/content.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var path = require('path')
+var fs = require('fs')
+
+module.exports = {
+
+
+  // load up data that can be used to create a nav for the content folder
+
+
+
+
+
+}
