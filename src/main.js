83b382a70b3bbc8bbcb31bc2e1e4136f9a84f98c
Dan Behlings <nevenall@hotmail.com>
2018-08-17 13:03:37 -0700

    adding analytics with page tracking

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import './vue-material'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+   id: 'UA-120807805-3',
+   router
+ })
 
 new Vue({
    router,
