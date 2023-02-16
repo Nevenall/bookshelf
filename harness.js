45cc47daca777aba31bf5e3e6c9a140a4c4b37e5
nevenall <nevenall@hotmail.com>
2023-02-15 19:11:20 -0800

    harness works pretty easily

--- harness.js
+++ harness.js
@@ -1,9 +1,9 @@
-import fs from 'fs/promises'
-import { compile, preprocess } from 'svelte/compiler'
+import fs from 'node:fs/promises'
+import { compile, preprocess } from 'npm:svelte/compiler'
 
 
 let source = await fs.readFile('src/book/03 Gearing Up.html')
 
 var result = compile(source.toString())
 
-var s = ''
+console.log(result)
